ba484c3974ec9f63c861be842c2b486c8b675f9e
Sidhant Jain <sidhantjain@Sidhants-MacBook-Pro.local>
2023-11-28 09:51:51 +0530

    Data Validation Added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from wine_quality_prediction import logger
 from wine_quality_prediction.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from wine_quality_prediction.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
 try:
@@ -12,4 +13,12 @@ except Exception as e:
     raise e
 
 
-
+STAGE_NAME = "Data Validation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataValidationTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
